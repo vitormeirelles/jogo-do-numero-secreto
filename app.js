407ef3566ab2a9d3dacf783d06bcc83d83cfa093
Vitor Hugo Meirelles <166816382+vitormeirelles@users.noreply.github.com>
2024-04-12 12:10:32 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female,', {rate:1.2}); 
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2}); 
 }
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do Número Secreto');
